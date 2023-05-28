acbcec2b49c0b4fcdd77b949e98a3d9665a9605f
Olevova <olevova1983@gmail.com>
2023-05-28 11:08:33 +0300

    add opportunity creat db and table if you dont creat or have

--- db.js
+++ db.js
@@ -1,13 +1,69 @@
-const {Pool} = require('pg');
-const pool = new Pool({
-    user: 'postgres',
-    password: 'home',
-    host:"localhost",
-    port:5432,
-    database: 'jet',
-    client_encoding: 'UTF8',
+const { Pool } = require("pg");
+const dbName = "jet";
+const connectionSettings = {
+  user: "postgres",
+  password: "home",
+  host: "localhost",
+  port: 5432,
+  client_encoding: "UTF8",
+};
+const serverPool = new Pool(connectionSettings);
 
-});
+// Функція яка буде створювати базу даних та таблицю 
+async function createDatabase() {
+  const serverClient = await serverPool.connect();
+  let dbClient;
+
+  try {
+    const checkDbQuery = `
+      SELECT 1 
+      FROM pg_catalog.pg_database 
+      WHERE datname = '${dbName}'
+    `;
+    const { rowCount } = await serverClient.query(checkDbQuery);
+    console.log(rowCount
+    );
+    if (rowCount === 0) {
+      // `CREATE DATABASE $1`, [dbName] - чомусь так не хоче працювати
+      await serverClient.query(`CREATE DATABASE ${dbName}`);
+
+      const dbPool = new Pool({
+        ...connectionSettings,
+        database: dbName,
+      });
+
+      dbClient = await dbPool.connect();
+
+      const createTableQuery = `
+      CREATE TABLE IF NOT EXISTS person (
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(255),
+        position VARCHAR(255),
+        moto TEXT
+      )
+    `;
+      await dbClient.query(createTableQuery);
+
+      console.log("Database created successfully!");
+    } else {
+      console.log("Database already exists.");
+    }
+  } catch (error) {
+    console.error("Error creating or checking database:", error);
+  } finally {
+    serverClient.release();
+    dbClient.release();
+  }
+}
+
 module.exports = {
-    query: (text, params) => pool.query(text, params)
-  };
+  query: (text, params) => {
+    const pool = new Pool({
+      ...connectionSettings,
+      database: dbName,
+    });
+
+    return pool.query(text, params);
+  },
+  createDatabase: createDatabase,
+};
--- index.js
+++ index.js
@@ -1,16 +1,20 @@
-const express = require('express');
-const cors = require('cors');
-const employeeRouter = require('./route/employee.route');
+const express = require("express");
+const cors = require("cors");
+const employeeRouter = require("./route/employee.route");
+const { createDatabase } = require("./db");
+
+// Перевіряємо наявність бази даних, якщо не має то створюємо її
+createDatabase()
+  .then(() => console.log("DB created"))
+  .catch((error) => console.error("Error:", error));
 
 const app = express();
 
-
 app.use(cors());
 app.use(express.json());
-app.use('/app',employeeRouter);
+app.use("/app", employeeRouter);
 
-const PORT = process.env.PORT||5050;
-app.listen(PORT, ()=>{
-    console.log(`its Start on ${PORT}`);
-})
-
+const PORT = process.env.PORT || 5050;
+app.listen(PORT, () => {
+  console.log(`its Start on ${PORT}`);
+});
